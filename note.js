6447f54d6fff584a9c2e6a91bc1b6eed77d7c507
Hüsne KÖMESÖĞÜTLÜ <96977741+HusneSakalli@users.noreply.github.com>
2024-09-19 21:27:38 +0300

    Add files via upload

---
+++ note.js
@@ -0,0 +1,20 @@
+//? javascript; yorumlanan bir dildir. Kod anlık olarak tarayıcı tarafından okunmasını ve çalıştırılmasına olanak tanır.
+// ? VERİ TİPLERİ(String,Number,boolean,array)
+// todo  1.String:Açılı penceremizde görüntülenmesini istediğimiz metindir. Ve veri türü tüm programlama dillerinde görmemize olak tanır.
+// todo  2.Number:sayılar alfabede kod için kullandığımızdan farklı sembollere sahip oldukları için, bilgisayara bu sayı siye söylememize gerek yoktur.
+Number=1;
+// todo 3.boolean :Doğruysa (true ) değerini döndür, yanlış ise (false ) değerini döndür diyor
+Boolean=false
+// todo 4.array= birden fazla deeğeri tek bir veri yapısında saklamaya yarayan bir veri tipidir.
+let meyveler = ["Elma","Armut","Muz"];
+meyveler.push("Porkal");
+console.log(meyveler[3]);
+// todo 5. Object(Nesne): javascript'te anahtar-değer çiftleri şeklinde veri saklayan bir veri tipidir.
+let kisi ={
+    ad:"Hüsne",
+    yas:24,
+    sehir:"Adana"
+};
+console.log(kisi.ad);
+console.log(kisi.yas);
+console.log(kisi.sehir);
